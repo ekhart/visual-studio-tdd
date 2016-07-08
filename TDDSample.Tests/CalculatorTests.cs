078994f4bbda2670586f637ee2a28b2d6bf9728a
ekhart <arturdebkowski@gmail.com>
2016-07-08 14:03:51 +0200

    RangeDivideTest - test infinity

--- TDDSample.Tests/CalculatorTests.cs
+++ TDDSample.Tests/CalculatorTests.cs
@@ -16,5 +16,29 @@ namespace TDDSample.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void RangeDivideTest()
+        {
+            var calculator = new Calculator();
+
+            for (var a = -100d; a < 100d; a++)
+            {
+                for (var b = -100d; b < 100d; b++)
+                {
+                    var expected = a/b;
+
+                    AssertRange(calculator, a, b, expected);
+                }
+            }
+        }
+
+        private static void AssertRange(Calculator calculator, double a, double b, double expected)
+        {
+            var actual = calculator.Divide(a, b);
+
+            Assert.AreEqual(expected, actual);
+            Assert.IsFalse(Double.IsInfinity(actual));
+        }
     }
 }
