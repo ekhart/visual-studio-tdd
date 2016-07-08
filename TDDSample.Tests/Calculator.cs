41eaede2f4055495a53b91cb322e8c39c77a3841
ekhart <arturdebkowski@gmail.com>
2016-07-08 13:59:13 +0200

    create unit test project first, then generate code for test

---
+++ TDDSample.Tests/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace TDDSample.Tests
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a/b;
+        }
+    }
+}
---
+++ TDDSample.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDSample.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void BasicDivideTest()
+        {
+            var calculator = new Calculator();
+            var expected = 4d;
+
+            var actual = calculator.Divide(20d, 5d);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
