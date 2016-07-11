0255ebf7cd47552b2d81d10ea08f465735d9ffe0
ekhart <arturdebkowski@gmail.com>
2016-07-11 12:50:18 +0200

    test shims

---
+++ MSFakesSample.Tests/DiagonizeShimsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+
+namespace MSFakesSample.Tests
+{
+    [TestClass]
+    public class DiagonizeShimsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DoomsDay_Test_Shims()
+        {
+            //using (ShimsContext.Create())
+            //{
+            //    ShimDateTime.NowGet = () => new DateTime(2012, 12, 21);
+                DiagonizeShims.DoomsDay();
+            //}
+        }
+    }
+}
---
+++ MSFakesSample/DiagonizeShims.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSFakesSample
+{
+    public class DiagonizeShims
+    {
+        public static void DoomsDay()
+        {
+            if (DateTime.Now.Equals(new DateTime(2012, 12, 21)))
+            {
+                throw new Exception("Time's Up!!! World will end now");
+            }
+
+            // other code
+        }
+    }
+}
